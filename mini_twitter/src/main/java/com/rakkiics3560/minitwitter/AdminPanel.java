5536052f5aaa2b1993430aab81e128141dbe2d69
RAKKII-san <schuang2001@gmail.com>
2022-11-03 00:00:36 -0700

    Initial class structure

---
+++ mini_twitter/src/main/java/com/rakkiics3560/minitwitter/AdminPanel.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rakkiics3560.minitwitter;
+
+/**
+ *
+ * @author Rakkii
+ */
+public class AdminPanel {
+    // Static instance
+    // Static getter
+    // Private constructor
+}
